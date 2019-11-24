c3ab2272a34d5fd05e35c53eb48674513e277b04
Rafał Szczuka <rafal.szczuka05@gmail.com>
2019-11-24 12:27:35 +0100

    units added

--- Node/node_zjazd3/task11-12/app.js
+++ Node/node_zjazd3/task11-12/app.js
@@ -3,15 +3,21 @@ const yargs = require("yargs");
 
 const args = yargs.argv;
 
-const city = args.city;
+const city = encodeURI(args.city);
 
 const weather = (city => {
   let url = `https://api.openweathermap.org/data/2.5/weather?APPID=0ed761300a2725ca778c07831ae64d6e&q=${city}`;
 
   request(url, (error, response, body) => {
     let data = JSON.parse(body);
-    console.log(`Actual weather in ${data.name}:`);
-    console.log(`Main parameters: ${JSON.stringify(data.main)}`);
-    console.log(`Wind: ${JSON.stringify(data.wind)}`);
+    console.log(`Actual weather in ${data.name}`);
+    console.log(
+      `\nMain parameters:\nTEMP: ${(Number(data.main.temp) - 273.15).toFixed(
+        2
+      )} C\nPRESSURE: ${data.main.pressure} HPA\nHUMIDITY: ${
+        data.main.humidity
+      }%`
+    );
+    console.log(`WIND SPEED: ${JSON.stringify(data.wind.speed)}`);
   });
 })(city);
