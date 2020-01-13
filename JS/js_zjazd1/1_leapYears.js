7372a16505674afd48e6a0f0f793934259e67222
Rafal Szczuka <rafal.szczuka05@gmail.com>
2020-01-13 23:01:41 +0100

    test5

--- JS/js_zjazd1/1_leapYears.js
+++ JS/js_zjazd1/1_leapYears.js
@@ -3,16 +3,17 @@
 // test2
 // test3
 // test4
+// test5
 
 let years = [1974, 1900, 1985, 2000];
 
-let leapYears = [];  // empty list, to store leap years
+let leapYears = []; // empty list, to store leap years
 
 // loop - iterate for every element of list, find values which modulo of 4 equals 0 (def of leap year)
-for(let i = 0; i < years.length; i++) {
-    if(years[i] % 4 == 0 && years[i] % 100 !== 0 || years[i] % 400 == 0) {
-        leapYears.push(years[i]);
-    }
+for (let i = 0; i < years.length; i++) {
+  if ((years[i] % 4 == 0 && years[i] % 100 !== 0) || years[i] % 400 == 0) {
+    leapYears.push(years[i]);
+  }
 }
 
 // print out list of years
