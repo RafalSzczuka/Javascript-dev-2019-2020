fd5f9cb83f2cd84b8b7588ecbc67a478416cc7c8
Rafal Szczuka <rafal.szczuka05@gmail.com>
2020-01-20 21:21:34 +0100

    simplified

--- React/shop/src/componets/Catalog/Catalog.jsx
+++ React/shop/src/componets/Catalog/Catalog.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import Search from "./Search/Search";
 import ProductList from "./ProductList/ProductList";
 
 const Catalog = () => {
   return (
     <div className="container">
       <h1 className="header-big">Catalog</h1>
-      <div className="catalog">
-        <Search />
-        <ProductList />
-      </div>
+      <ProductList />
     </div>
   );
 };
