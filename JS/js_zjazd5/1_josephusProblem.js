a66b4a7a00f8a8eaead5645dea18c3c51be9d6fa
Rafał Szczuka <rafal.szczuka05@gmail.com>
2020-02-04 21:42:06 +0100

    new solution added

--- JS/js_zjazd5/1_josephusProblem.js
+++ JS/js_zjazd5/1_josephusProblem.js
@@ -27,56 +27,80 @@ https://www.youtube.com/watch?v=uCsD3ZGzMgE
 //--------------------------------------------------------------------
 
 // second solution
+// recoursive function (envokes itself)
 
-// const resolveJosephus = (soldiers, step) => {
+// const resolveJosephus = soldiers => {
 //   if (soldiers == 1) return 1;
 //   else {
-//     return ((resolveJosephus(soldiers - 1, 2) + 2 - 1) % soldiers) + 1;
+//     return ((resolveJosephus(soldiers - 1) + 1) % soldiers) + 1;
 //   }
 // };
 
-// console.log(resolveJosephus(10, 2));
+// console.log(resolveJosephus(7));
 //--------------------------------------------------------------------
 
 // third solution
-class Soldier {
-  constructor(position, name) {
-    this.position = position;
-    this.name = name;
+// class Soldier {
+//   constructor(position, name) {
+//     this.position = position;
+//     this.name = name;
+//   }
+// }
+
+// const names = [
+//   "Augustus",
+//   "Maximus",
+//   "Octavian",
+//   "Gaius",
+//   "Flavius",
+//   "Titus",
+//   "Josephus"
+// ];
+// let soldiers = [];
+
+// for (let i = 0; i < 7; i++) {
+//   soldiers.push(new Soldier(i + 1, names[i]));
+// }
+
+// soldiers.forEach(soldier => {
+//   console.log(`Soldier: ${soldier.name} at ${soldier.position} position`);
+// });
+// console.log("");
+// for (let i = 0; soldiers.length != 1; i++) {
+//   whosKilled = i + 1;
+//   console.log(
+//     `${soldiers[i].name} at ${soldiers[i].position} position kills ${soldiers[whosKilled].name} at ${soldiers[whosKilled].position} position`
+//   );
+//   soldiers.splice(whosKilled, 1);
+//   soldiers.push(soldiers[i]);
+//   soldiers.splice(i, 1);
+
+//   i = -1;
+// }
+
+// console.log(
+//   `\nLast survivor is ${soldiers[0].name}, at position number ${soldiers[0].position}`
+// );
+
+//fourth solution
+const josephusResult = howMuchSoldiers => {
+  let soldiers = [];
+
+  for (let i = 1; i <= howMuchSoldiers; i++) {
+    soldiers.push(i);
   }
-}
 
-const names = [
-  "Augustus",
-  "Maximus",
-  "Octavian",
-  "Gaius",
-  "Flavius",
-  "Titus",
-  "Josephus"
-];
-let soldiers = [];
+  while (soldiers.length > 1) {
+    for (let i = 0; i < soldiers.length; i++) {
+      let whosKilled = i + 1;
+      if (whosKilled >= soldiers.length) whosKilled = 0;
 
-for (let i = 0; i < 7; i++) {
-  soldiers.push(new Soldier(i + 1, names[i]));
-}
+      console.log(`${soldiers[i]} killed ${soldiers[whosKilled]}`);
 
-soldiers.forEach(soldier => {
-  console.log(`Soldier: ${soldier.name} at ${soldier.position} position`);
-});
-console.log("");
-for (let i = 0; soldiers.length != 1; i++) {
-  whosKilled = i + 1;
-  console.log(
-    `${soldiers[i].name} at ${soldiers[i].position} position kills ${soldiers[whosKilled].name} at ${soldiers[whosKilled].position} position`
-  );
-  soldiers.splice(whosKilled, 1);
-  soldiers.push(soldiers[i]);
-  soldiers.splice(i, 1);
+      soldiers.splice(whosKilled, 1);
+    }
+  }
+  console.log(`Soldier ${soldiers[0]} survived`);
+};
 
-  i = -1;
-}
-
-console.log(
-  `\nLast survivor is ${soldiers[0].name}, at position number ${soldiers[0].position}`
-);
+josephusResult(7);
