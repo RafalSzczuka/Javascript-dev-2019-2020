90f3294b7c5d9bc8bdf451c6c6da2fda5b132046
Rafał Szczuka <rafal.szczuka05@gmail.com>
2020-02-21 10:27:48 +0100

    finished

--- JS/SEM2/zjazd1/02_guessingGame.js
+++ JS/SEM2/zjazd1/02_guessingGame.js
@@ -1,12 +1,49 @@
-let guessTries = 0;
 let secretNumber = 0;
 let range = [];
+let guessNumber;
+let guessTries = 0;
 
-const rangeGenerator = () => {
+// iife function sets an array of numbers from 1 to 100
+const rangeGenerator = (() => {
   for (let i = 1; i <= 100; i++) {
     range.push(i);
   }
+})();
+
+// function to choose random number from array provided
+const randomNumber = numbers => {
+  let randomIndex = Math.floor(Math.random() * numbers.length);
+  return numbers[randomIndex];
 };
 
-rangeGenerator();
-console.log(range);
+// set secret number
+secretNumber = randomNumber(range);
+
+// algorithm with slicing range of numbers
+const guessingAlghoritm = numbers => {
+  console.log(`Secret number: ${secretNumber}\n`);
+
+  while (numbers.length != 1) {
+    guessNumber = randomNumber(numbers);
+    console.log(`Guess number: ${guessNumber}`);
+    let numIndex = numbers.indexOf(guessNumber);
+    guessTries += 1;
+
+    if (guessNumber === secretNumber) {
+      console.log(`\nBingo!\nTries number: ${guessTries}`);
+      return;
+    }
+
+    if (guessNumber < secretNumber) {
+      numbers = numbers.slice(numIndex + 1, numbers.length);
+    } else {
+      numbers = numbers.slice(0, numIndex);
+    }
+    if (numbers.length === 1) {
+      console.log(`\nFailed :(\nTries number: ${guessTries}`);
+      return;
+    }
+  }
+};
+
+guessingAlghoritm(range);
