23585782df56bc3732668895a79e8e68ee342d95
Rafał Szczuka <rafal.szczuka05@gmail.com>
2020-02-23 08:48:13 +0100

    validation added

--- JS/SEM2/zjazd1/04_fibonacci.js
+++ JS/SEM2/zjazd1/04_fibonacci.js
@@ -8,20 +8,29 @@ The first few are therefore 1, 1, 1+1=2, 1+2=3, 2+3=5, 3+5=8.
 class Fibonacci {
   constructor(n) {
     this.n = n;
-    this.sequence = [1, 1];
+    this.sequence = [];
 
-    while (this.sequence.length < n) {
-      let nextNumber =
-        this.sequence[this.sequence.length - 2] +
-        this.sequence[this.sequence.length - 1];
+    if (n <= 0) {
+      return (this.sequence = [0]);
+    } else if (n === 1) {
+      return (this.sequence = [1]);
+    } else if (n === 2) {
+      return (this.sequence = [1, 1]);
+    } else {
+      this.sequence = [1, 1];
+      while (this.sequence.length < n) {
+        let nextNumber =
+          this.sequence[this.sequence.length - 2] +
+          this.sequence[this.sequence.length - 1];
 
-      this.sequence.push(nextNumber);
+        this.sequence.push(nextNumber);
+      }
     }
 
     return this.sequence;
   }
 }
 
-let fib = new Fibonacci(20);
+let fib = new Fibonacci(10);
 
-console.log(fib);
+fib.forEach(num => console.log(num));
