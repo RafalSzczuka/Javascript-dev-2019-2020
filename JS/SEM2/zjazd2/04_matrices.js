cbc3ee647baa348e012a1bed51146f70aa2cae45
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-04-05 01:41:32 +0200

    Update 04_matrices.js
    test commit

--- JS/SEM2/zjazd2/04_matrices.js
+++ JS/SEM2/zjazd2/04_matrices.js
@@ -1,4 +1,4 @@
-// Write a code that multiplies two matrices together. Dimension validation required.
+// Write a code that multiplies two matrices together. Dimension validation required
 /*      2 1 3
        -1 4 0
 
